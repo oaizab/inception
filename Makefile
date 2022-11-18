2746e6d278dc82ce977c8a1c2fb07eba9ae43137
Omar Aizab <omaraizab98@gmail.com>
2022-11-18 16:45:49 +0100

    fix css on wordpress

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ all: prune reload
 stop:
 	docker compose -f srcs/docker-compose.yml down
 
+up:
+	docker compose -f srcs/docker-compose.yml up -d
+
 clean: stop
 	docker system prune -af
 
@@ -15,5 +18,7 @@ prune: stop
 
 reload:
 	docker compose -f srcs/docker-compose.yml up -d --build
+
+re: fclean all
 
 .PHONY: stop clean fclean prune reload all
--- srcs/requirements/wordpress/config/config.sh
+++ srcs/requirements/wordpress/config/config.sh
@@ -20,7 +20,7 @@ if [ ! -f "/var/www/html/index.html" ]; then
 	wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 	# wp theme install inspiro --activate --allow-root
-	wp theme install hello-elementor --activate --allow-root
+	# wp theme install hello-elementor --activate --allow-root
 
 	# add redis
 fi
