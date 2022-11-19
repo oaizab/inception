f69522ef0fd219099e427a77d418b37a59446915
Omar Aizab <omaraizab98@gmail.com>
2022-11-19 02:36:07 +0100

    setup redis and portainer

--- srcs/requirements/wordpress/config/config.sh
+++ srcs/requirements/wordpress/config/config.sh
@@ -13,6 +13,8 @@ if [ ! -f "/var/www/html/index.html" ]; then
 	mv /tmp/index.html /var/www/html/index.html
 
 	# setup adminer
+	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
+    wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
 
 	# wordpress
 	wp core download --allow-root
@@ -23,6 +25,15 @@ if [ ! -f "/var/www/html/index.html" ]; then
 	# wp theme install hello-elementor --activate --allow-root
 
 	# add redis
+	sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );" /var/www/html/wordpress/wp-config.php
+	sed -i "41i define( 'WP_REDIS_PORT', 6379 );" /var/www/html/wordpress/wp-config.php
+	sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );" /var/www/html/wordpress/wp-config.php
+	sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );" /var/www/html/wordpress/wp-config.php
+	sed -i "44i define( 'WP_REDIS_DATABASE', 0 );" /var/www/html/wordpress/wp-config.php
+
+	wp plugin install redis-cache --activate --allow-root
+	wp plugin update --all --allow-root
+
 fi
 
 /usr/sbin/php-fpm7 -F -R
