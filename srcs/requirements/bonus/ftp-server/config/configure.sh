242a3d6fad695156fff75b154b4591f060329e29
Omar Aizab <omaraizab98@gmail.com>
2022-11-22 00:04:16 +0100

    add ftp-server

---
+++ srcs/requirements/bonus/ftp-server/config/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -f /etc/vsftpd/vsftpd.conf.bak ]; then
+
+	mkdir -p /var/www/html
+
+	cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
+	mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
+
+	adduser $FTP_USR --disabled-password
+	echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
+	chown -R $FTP_USR:$FTP_USR /var/www/html
+
+	echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
+
+fi
+
+echo "FTP started on :21"
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
